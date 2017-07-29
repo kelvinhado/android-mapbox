a6d87ca62060e7de8e61285b46b5b807a5ad5547
kelvinhado <kelvinhado@outlook.com>
2017-07-29 18:59:33 +0200

    localize user

--- mapbox/app/src/main/java/com/kelvinhado/mapbox/MapFragment.java
+++ mapbox/app/src/main/java/com/kelvinhado/mapbox/MapFragment.java
@@ -1,20 +1,36 @@
 package com.kelvinhado.mapbox;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Icon;
+import com.mapbox.mapboxsdk.annotations.IconFactory;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MapFragment extends Fragment {
     private MapView mapView;
+    private LatLng userPosition;
 
     public MapFragment() {
     }
@@ -31,8 +47,93 @@ public class MapFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         mapView = (MapView) view.findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                IconFactory iconFactory = IconFactory.getInstance(getActivity());
+                Icon icon = iconFactory.fromResource(R.drawable.map_marker);
+                mapboxMap.addMarker(new MarkerOptions()
+                        .title(getString(R.string.map_location_me))
+                        .position(new LatLng(48.85819, 2.29458))
+                        .icon(icon)
+                );
+                if(mapboxMap.getMyLocation() != null) {
+                    userPosition = new LatLng(mapboxMap.getMyLocation().getLatitude(),
+                            mapboxMap.getMyLocation().getLongitude());
+                    mapView.setCameraDistance(10);
+                    CameraPosition position = new CameraPosition.Builder()
+                            .target(userPosition)
+                            .build();
+                    mapboxMap.addMarker(new MarkerOptions()
+                            .title(getString(R.string.map_location_me))
+                            .position(userPosition)
+                            .icon(icon)
+                    );
+                    mapboxMap.animateCamera(CameraUpdateFactory
+                            .newCameraPosition(position), 7000);
 
+                }
+
+            }
+        });
+
+        // check location permission
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+                checkLocationPermission();
+        }
     }
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 1993;
+    private void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(getActivity())
+                        .setTitle(R.string.permission_alert_title)
+                        .setMessage(R.string.permission_alert_location)
+                        .setPositiveButton(R.string.permission_alert_button_ok, 
+                                new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(getActivity(),
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION );
+                            }
+                        })
+                        .create()
+                        .show();
+            } else {
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION );
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(getActivity(),
+                            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        // permission granted
+                    }
+
+                } else {
+                    // permission denied
+                }
+                return;
+            }
+        }
+    }
+
+
 
     @Override
     public void onStart() {
